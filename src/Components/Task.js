5875d22f7413ae302160dc2970a4337b34bc901e
Cathy Le <cathy347le@gmail.com>
2022-01-28 17:07:00 -0500

    add close X icon

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,7 +1,15 @@
+import { FaTimes } from "react-icons/fa";
+
 function Task({ task }) {
   return (
     <div className="task-item">
-      <h5>{task.title}</h5>
+      <h5>
+        {task.title}{" "}
+        <FaTimes
+          className="close-icon"
+          style={{ color: "red", cursor: "pointer" }}
+        />
+      </h5>
       <p>{task.date}</p>
       <p>Completed: </p>
     </div>
