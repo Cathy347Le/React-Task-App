bc2a21afcbdbe2029e678a32bf8b3af7dfa3e462
Cathy Le <cathyle@cathys-mbp.lan>
2022-01-28 15:04:58 -0500

    style button component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ function Header({ name }) {
   return (
     <header className="app-header">
       <h1>{name}'s Task Tracker</h1>
-      <Button color="blue" text="Add New Ask" />
+      <Button color="#5f27cd" text="Add New Ask" />
     </header>
   );
 }
