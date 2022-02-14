23c8928f0c91d8d38364d1f84efd61d526c05cdb
Cathy Le <cathy347le@gmail.com>
2022-02-13 22:27:17 -0500

    update styling

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -3,15 +3,17 @@ import Button from './Button';
 
 function TaskList({ tasks, onDelete, onToggleCompleted, onDeleteAllTask }) {
   return (
-    <div className="task-list-container">
-      {tasks.map((task) => (
-        <Task
-          key={task.id}
-          task={task}
-          onDelete={onDelete}
-          onToggleCompleted={onToggleCompleted}
-        />
-      ))}
+    <div className="task-list-wrapper">
+      <div className="task-list-container">
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            onDelete={onDelete}
+            onToggleCompleted={onToggleCompleted}
+          />
+        ))}
+      </div>
       <Button text="Delete All Task" onClick={() => onDeleteAllTask(tasks)} />
     </div>
   );
