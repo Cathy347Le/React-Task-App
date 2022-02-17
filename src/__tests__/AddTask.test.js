aa1c9215c5b0fc7a7c466ccb8098e61e28f56902
Cathy Le <cathy347le@gmail.com>
2022-02-17 15:41:06 -0500

    work on steps to test askTask form functionality

--- src/__tests__/AddTask.test.js
+++ src/__tests__/AddTask.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import AddTask from '../Components/AddTask';
+import TaskList from '../Components/TaskList';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // test('submit button should disabled at initial render', () => {
 //   render(<AddTask />);
@@ -34,4 +36,27 @@ test('submit button should be disabled at initial render and enabled when title 
 
   userEvent.selectOptions(screen.getByRole('combobox'), 'Monday');
   expect(screen.getByRole('button', { name: /add new task/i })).toBeEnabled();
+});
+
+test('check askTask form adds new task when submit button is clicked', () => {
+  //Render AskTaskform and TaskList
+  render(
+    <Router>
+      <AddTask />
+      <TaskList
+        tasks={[
+          {
+            title: 'Give Gouda a bath',
+            date: 'Friday',
+            completed: false,
+            id: 8,
+          },
+        ]}
+      />
+    </Router>
+  );
+  //Capture askform title and select date and submit Button
+  //Add userEvent for title, date, and button click
+  //Check Task List for the new Task - search for title and date and reminder icon
+  screen.debug();
 });
